ed84f0974d89398f2ec49227e2212d5980172213
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-04-30 14:51:57 -0300

    refactors routes to use resources method.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    get '/pokemon', to: 'pokemons#index'
-    get '/pokemon/:id', to: 'pokemons#show'
-    post '/pokemon', to: 'pokemons#create'
-    put '/pokemon/:id', to: 'pokemons#update'
-    delete '/pokemon/:id', to: 'pokemons#destroy'
+    resources :pokemon, controller: 'pokemons'
   end
 end
