04520897d27f27de8f5738e6b4631b34e1341130
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-05-02 19:53:54 -0300

    creates presence validation of name in Poketype.

--- app/models/poketype.rb
+++ app/models/poketype.rb
@@ -1,3 +1,4 @@
 class Poketype < ApplicationRecord
   has_and_belongs_to_many :pokemons
+  validates :name, presence: true
 end
--- spec/models/poketype_spec.rb
+++ spec/models/poketype_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Poketype, type: :model do
   it { is_expected.to have_and_belong_to_many(:pokemons) }
+  it { is_expected.to validate_presence_of(:name) }
 end
